db0f961a8287a96e8e9a7077c699e3bcdff1b088
mitugui <cristian.mitugui04@gmail.com>
2024-11-05 22:30:10 -0300

    feat: add CORS configuration to allow requests from frontend - Allows CORS for all paths in the application - Restricts requests to origins specified in cors.allowed.origins environment variable - Permits GET HTTP method

---
+++ src/main/java/br/com/alura/screenmatch_frases/config/CorsConfiguration.java
@@ -0,0 +1,19 @@
+package br.com.alura.screenmatch_frases.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Value("${cors.allowed.origins}")
+    private String allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET");
+    }
+}
